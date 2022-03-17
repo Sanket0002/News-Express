46a2cc9743c5324639870e16061f79582a7d551b
sanket <sshubham.sanket@gmail.com>
2022-03-17 11:16:18 +0530

    initial commit

---
+++ src/components/NewsItems.js
@@ -0,0 +1,47 @@
+import React from "react";
+
+const NewsItems =(props)=> {
+    let { title, description, imageUrl, newsUrl, author, date,source } = props;
+    return (
+      <div>
+        <div className="card my-3">
+          <div style={{display:'flex',justifyContent:'flex-end',position:'absolute',right:'0' }}>
+        <span className=" badge rounded-pill bg-danger">{source}</span>
+          </div>
+          <img
+            src={
+              !imageUrl
+                ? "https://images.indianexpress.com/2022/01/omicron-13.jpg"
+                : imageUrl
+            }
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body">
+            <h5 className="card-title">
+              {title}
+            </h5>
+            <p className="card-text">{description}</p>
+            <p className="card-text">
+              <small className="text-muted">
+                By {!author ? "Unknown" : author} On
+                {new Date(date).toGMTString()}
+              </small>
+            </p>
+            <button>
+              <a
+                rel="noreferrer"
+                href={newsUrl}
+                target="_blank"
+                className="btn btn-sm btn dark"
+              >
+                Go To News
+              </a>
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+export default NewsItems;
